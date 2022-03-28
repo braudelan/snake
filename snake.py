a3d01fe4de2b009ff0debd7e5a2879c4acea74a6
Your Name <you@example.com>
2022-03-28 16:07:05 +0300

    wrote DIRECTIONS as dict, separate functions for snake and fruit

--- snake.py
+++ snake.py
@@ -8,7 +8,12 @@ from collections import namedtuple
 FRUIT = -1
 
 # possible directions for the snake
-DIRECTIONS = ['W', 'N', 'E', 'S']
+DIRECTIONS = {
+    'W': 0,
+    'N': 1,
+    'E': 2,
+    'S': 3
+}
 # map DIRECTION to MOVEMENT element that will be added to current head.
 #  e.g. if head is located at (2, 1), i.e. 3rd row, 2nd column,
 #  and we get an input of 'N',
@@ -21,7 +26,7 @@ MOVEMENTS = {
 }
 
 
-def get_fruit_locations(screen_size=20, fruit=1) -> list:
+def get_fruit(screen_size=20, fruit=1) -> list:
     """
     return a list of random fruit locations
 
@@ -38,59 +43,55 @@ def get_fruit_locations(screen_size=20, fruit=1) -> list:
     return fruit_locations
 
 
-def get_initial_state(screen_size, snake_length):
-    '''get the initial state of the screen, snake and fruit'''
-    def get_initial_snake():
-        '''get the initial locations of snake and direction of the head'''
-        head = (screen_size // 2, screen_size-2)
-        locations = [  # use numpy arrays to allow vector addition
-            np.arrdkay(head[0], head[1]+1),
-            np.array(head)
-        ]
-        direction = 'W'
-        InitialSnake = namedtuple(
-            'Snake',
-            ['locations', 'direction']
-        )
-        return InitialSnake(locations, direction)
+# def get_initial_state(screen_size, snake_length):
+#     '''get the initial state of the screen, snake and fruit'''
 
-    # initialize screen, snake and fruit
-    # screen = np.zeros(screen_size, screen_size)
-    snake = get_initial_snake()
-    fruit = get_fruit_location()  # default number of fruit is 1
+#     snake = get_initial_snake()
+#     fruit = get_fruit_locations()  # default number of fruit is 1
 
-    # # mark initial snake and fruit locations on screen
-    # #  
-    # i = 1
-    # for location in snake.locations:
-    #     screen[location] = i
-    #     i += 1
-    # for location in fruit.locations:
-    #     screen[location] = FRUIT
-    InitialState = namedtuple(
-        'State', 
-        ['snake', 'fruit']
+#     InitialState = namedtuple(
+#         'State', 
+#         ['snake', 'fruit']
+#     )
+#     return InitialState(snake=snake, fruit=fruit)
+
+def get_initial_snake(screen_size):
+    '''get the initial locations of snake and direction of the head'''
+    head = (screen_size // 2, screen_size-2)
+    locations = [  # use numpy arrays to allow vector addition
+        np.array([head[0], head[1] + 1]),
+        np.array(head)
+    ]
+    direction = 'W'
+    InitialSnake = namedtuple(
+        'Snake',
+        ['locations', 'direction']
     )
-    return InitialState(snake=snake, fruit=fruit)
+    return InitialSnake(locations, direction)
 
 
-def get_next_fruit(screen_size) -> list:
-    # retun a list of random fruit locations based on screen size, 
-    return fruit
-
-
-def get_next_direction(current_direction, user_input) -> str:
-    # if direction opposite stay with same direction
-    # if no input stay with same direction
-    # otherwise return input
+def get_next_direction(current_direction,
+                       direction_from_input) -> str:
+    # if current direction is opposite to input direction
+    #  or no input stay with current direction
+    # otherwise return input direction
+    sum_directions = (
+        DIRECTIONS[current_direction]
+        + DIRECTIONS[direction_from_input]
+    )
+    if direction_from_input and sum_directions % 2 != 0:
+        next_direction = direction_from_input
+    else:
+        next_direction = current_direction
     return next_direction
 
 
-def get_next_snake(current_state, get_input) -> namedtuple:
-    user_input = get_input()
-    current_locations = current_state.snake.locations
+def get_next_snake(current_snake,
+                   direction_from_input) -> namedtuple:
+    user_input = direction_from_input
+    current_locations = current_snake.locations
     next_direction = get_next_direction(
-        current_state.snake.direction,
+        current_snake.direction,
         user_input
     )
     next_head = (
@@ -98,6 +99,7 @@ def get_next_snake(current_state, get_input) -> namedtuple:
         + MOVEMENTS[next_direction]
     )
     next_locations = current_locations + [next_head]
+    next_locations.pop(0)
     Snake = namedtuple(
         'Snake',
         ['locations', 'direction']
@@ -106,4 +108,5 @@ def get_next_snake(current_state, get_input) -> namedtuple:
         locations=next_locations,
         direction=next_direction
     )
-    
+
+
